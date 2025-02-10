65c3fd09c44ea161d0937d9577ddc5aad898bc2b
Arbaj Firoj Shaikh <arbajshk@localhost.localdomain>
2025-02-10 23:06:49 +0530

    DSU

---
+++ Graph/DSU.cpp
@@ -0,0 +1,37 @@
+struct UnionFind {
+	int n;
+	vector<int> parent;
+	vector<int> size;
+
+    multiset<int>st;
+
+	UnionFind(int n) {
+		parent.resize(n);
+		size.resize(n, 1);
+		for (int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+	}
+
+	int get(int a) {
+		return parent[a] = (parent[a] == a ? a : get(parent[a]));
+	}
+
+    //to get all available sizes in graph min or max
+    void merge(int a,int b){
+        st.erase(st.find(size[get(a)]));
+        st.erase(st.find(size[get(b)]);
+        st.insert(size[get(a)]+size[get(b)]);
+    }
+
+	void union(int a, int b) {
+		a = get(a);
+		b = get(b);
+		if (a == b) return;
+
+		if (size[a] < size[b]) swap(a, b);
+		parent[b] = a;
+        merge(a,b);
+		size[a] += size[b];
+	}
+};
